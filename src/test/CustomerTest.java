7d3d32ceffbe0d56cb29ae631afb68100919bc7f
Rifleborn <49074281+Rifleborn@users.noreply.github.com>
2020-10-23 16:39:31 +0300

    Update CustomerTest.java

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
+
 import domain.Customer;
-/**
- *
- * @author User
- */
+
 public class CustomerTest {
 	
 	public static void main(String[] args) {
 		Customer customer = new Customer();
-		customer.displayCustomerInfo();
+		
+                customer.setId(1);
+                System.out.println(customer.getId());
+                customer.setIsNew(true);
+                System.out.println(customer.IsNew());
+                customer.setTotal(1001);
+                System.out.println( customer.getTotal());        
 	}
 }
